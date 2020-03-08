9bf9bf1cf7b0eea1364dc6eb13f05abfd6837f5b
florianipate <florian.ipate@gmail.com>
2020-03-08 18:42:06 +0000

    created the home page and the folder structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import HomePage from './pages/homepage/homepage.component.jsx';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     
+    <div>
+      <HomePage />
     </div>
   );
 }
---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './homepage.style.scss';
+
+const HomePage = () => (
+<div className = "container">
+    Hell from the Home Page!
+</div>
+);
+
+export default HomePage;
