dcce42d1be922723adda7a1ec6eb533c01d3f9c0
florianipate <florian.ipate@gmail.com>
2020-03-12 09:36:31 +0000

    update the section content

--- src/components/section-card/section-card.component.jsx
+++ src/components/section-card/section-card.component.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import './section-card.style.scss';
 
-const SectionCard = () => (
+const SectionCard = ({title, imageUrl}) => (
     <div className="section-inner">
-        <img src={require('../images/image3.jpg')} width="100%" height="100%"  alt="image3"/>
+        <div className = "background-image">
+            <img src={imageUrl} width="100%" height="100%"  alt="image1"/>
+        </div>
+        <h1 className = "title">{title}</h1>
     </div>
 );
 export default SectionCard;
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './homepage.style.scss';
 import Hero from '../../components/hero/hero.component.jsx';
+import SectionBlock from '../../components/section-block/section-block.component.jsx';
 
 const HomePage = () => (
 <div className = "container">
     <Hero />
+    <SectionBlock />
 </div>
 );
 
