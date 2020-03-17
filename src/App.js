aa2687812d1168945f97f1d51c4513b6dfedd82d
florianipate <florian.ipate@gmail.com>
2020-03-17 16:51:33 +0000

    create boys page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Switch, Route } from 'react-router-dom';
 import  MenPage from './pages/menpage/menpage.component.jsx';
 import WomenPage from './pages/womenpage/womenpage.component.jsx';
 import GirlsPage from './pages/girls-page/girls-page.component.jsx';
+import BoysPage from './pages/boys-page/boys-page.component.jsx';
 import './App.css';
 
 
@@ -13,11 +14,12 @@ function App() {
   return (
     <div>
       <Switch>
-        <Route exact path= '/' component= {HomePage} />
-        <Route exact path= '/women' component= {WomenPage} />
-        <Route path= '/men' component= {MenPage} />
-        <Route path = '/women' component = {WomenPage} />
-        <Route path = '/girls' component = {GirlsPage} />
+        <Route exact path= '/' component = {  HomePage } />
+        <Route exact path= '/women' component = { WomenPage } />
+        <Route path= '/men' component= { MenPage } />
+        <Route path = '/women' component = { WomenPage } />
+        <Route path = '/girls' component = { GirlsPage } />
+        <Route path = '/boys' component = { BoysPage } />
       </Switch>
     </div>
   );
